dffc45d0cf703f8933a2f5ad75cdfcb7c25736f6
l8827974 <95362163+l8827974@users.noreply.github.com>
2021-12-20 22:00:10 +0800

    Update qiandao.py

--- qiandao.py
+++ qiandao.py
@@ -56,13 +56,13 @@ def setJwsession(self, jwsession):
 
 # 获取JWSESSION
 def getJwsession(self):
-    if not self.jwsession:  # 读取cache中的配置文件
+    if not jwsession:  # 读取cache中的配置文件
         data = utils.processJson(".cache/cache.json").read()
-        self.jwsession = data['jwsession']  
-    return self.jwsession
+        jwsession = data['jwsession']  
+    return jwsession
 
 # 我在校园jwsession,抓包获得
-jwsession = self.getJwsession()
+#jwsession = self.getJwsession()
 # 在pushplus网站中可以找到 http://pushplus.hxtrip.com/
 pushplus_token = 'cd51aa7b1a2f44259e7630ad316dfa64'
 
