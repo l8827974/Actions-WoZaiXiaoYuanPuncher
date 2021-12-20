0b08d51bd410abe093dbe3babefc6e22744506cf
l8827974 <95362163+l8827974@users.noreply.github.com>
2021-12-21 01:09:35 +0800

    Update qiandao.py

--- qiandao.py
+++ qiandao.py
@@ -3,8 +3,10 @@ import requests
 import json
 import time
 
+data = utils.processJson(".cache/cache.json").read()
+jwsession = data['jwsession']  
 # 我在校园jwsession,抓包获得
-jwsession = '43f311ed4d094adcbc6e5138caec603e'
+# jwsession = '43f311ed4d094adcbc6e5138caec603e'
 # 在pushplus网站中可以找到 http://pushplus.hxtrip.com/
 pushplus_token = 'cd51aa7b1a2f44259e7630ad316dfa64'
 
