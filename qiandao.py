b6a4d7ea402703bbce68615ea86538f93ed57742
l8827974 <95362163+l8827974@users.noreply.github.com>
2021-12-20 21:28:55 +0800

    Update qiandao.py

--- qiandao.py
+++ qiandao.py
@@ -62,6 +62,7 @@ def getJwsession(self):
     return self.jwsession
 
 # 我在校园jwsession,抓包获得
+jwsession = self.getJwsession()
 # 在pushplus网站中可以找到 http://pushplus.hxtrip.com/
 pushplus_token = 'cd51aa7b1a2f44259e7630ad316dfa64'
 
